7429df5c17af084f72adea1ca580173a2c1416a6
mdfazil <fazilsaifi250@gmail.com>
2025-03-01 16:35:41 +0530

    Fixed CORS issue for Vercel frontend

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-      origin: ["https://study-point-kappa.vercel.app"],
+      origin: "https://study-point-kappa.vercel.app",
       methods: "GET,POST,PUT,DELETE",
       credentials: true, 
     })
